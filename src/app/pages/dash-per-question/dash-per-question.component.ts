2f2d9f952989ef59024948d8bdf93ee32274f329
Bruno Rocha <brunolongo333@hotmail.com>
2022-10-22 14:56:40 -0300

    Integração dos dash com a API

--- src/app/pages/dash-per-question/dash-per-question.component.ts
+++ src/app/pages/dash-per-question/dash-per-question.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { UserLoggedService } from 'src/app/shared/services/user-logged/user-logged.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-dash-per-question',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashPerQuestionComponent implements OnInit {
 
-  constructor() { }
+  forms: any[] = []
 
-  ngOnInit(): void {
+  constructor(
+    private http: HttpClient,
+    public userLoggedService: UserLoggedService
+  ) { }
+
+  async ngOnInit(): Promise<void> {
+    await this.getForms();
+    await this.getQuestions();
   }
+
+  async getForms(){
+    this.forms = await this.http.get<any>(`${environment.api}/forms/perUser/${this.userLoggedService.idUsuario}`).toPromise();
+    console.log(this.forms)
+  }
+
+  async getQuestions(){
+    for (const [index, form] of this.forms.entries()) {
+      this.forms[index].questions = await this.http.get<any>(`${environment.api}/questions/perForm?form=${form.id}`).toPromise();
+    }
+    console.log(this.forms)
+  }
+
 
 }
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import Chart from 'chart.js/auto';
+import { UserLoggedService } from 'src/app/shared/services/user-logged/user-logged.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-dashboard',
@@ -13,13 +17,37 @@ export class DashboardComponent implements OnInit {
   @ViewChild('myChartPieEstadoCivil', {static: true}) myChartPieEstadoCivil: ElementRef;
   @ViewChild('myChartPieGenero', {static: true}) myChartPieGenero: ElementRef;
 
-  constructor() { }
+  public dash: any = {};
+  public id: string | null;
 
-  ngOnInit(): void {
-    //const ctx = document.getElementById('myChart').getContext('2d');
+  constructor(
+    private http: HttpClient,
+    private route: ActivatedRoute,
+    public userLoggedService: UserLoggedService
+  ) { }
+
+  async ngOnInit(): Promise<void> {
+    this.id = this.route.snapshot.paramMap.get('id');
+    await this.getDataDash();
+    this.createDashs();
   }
 
-  ngAfterViewInit(){
+  async getDataDash(){
+    this.dash = await this.http.get<any>(`${environment.api}/dashboards/${this.id}`).toPromise();
+  }
+
+  generateColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    
+    return color;
+  }
+
+  createDashs(){
     this.chartPieNumerica();
     this.chartPieState();
     this.chartPieEstadoCivil();
@@ -27,14 +55,18 @@ export class DashboardComponent implements OnInit {
   }
 
   chartPieNumerica(){
+    const labelDash = Object.keys(this.dash.results[0][0].answer)
+    const dataDash = Object.values(this.dash.results[0][0].answer)
+    const colorDash = Array.from({length: labelDash.length}, (v, k) => k+1).map( num => this.generateColor())
+
     const ctx = new Chart(this.myChartPieNumerica.nativeElement, {
       type: 'pie',
       data: {
-        labels: ["10","9", "8", "7", "6"],
+        labels: labelDash,
         datasets: [
           {
-            data: [43, 20, 12, 36, 25],
-            backgroundColor: ['#36a2eb', 'purple', '#367E18', '#0F3460', '#ff6384'],
+            data: dataDash,
+            backgroundColor: colorDash,
           }
         ]
       },
@@ -55,14 +87,18 @@ export class DashboardComponent implements OnInit {
 
  
   chartPieState(){
+    const labelDash = Object.keys(this.dash.results[0][1].state)
+    const dataDash = Object.values(this.dash.results[0][1].state)
+    const colorDash = Array.from({length: labelDash.length}, (v, k) => k+1).map( num => this.generateColor())
+
     const ctx = new Chart(this.myChartPieState.nativeElement, {
       type: 'pie',
       data: {
-        labels: ["RO","SP", "RJ", "SC", "BH"],
+        labels: labelDash,
         datasets: [
           {
-            data: [33, 10, 6, 24, 18],
-            backgroundColor: ['#36a2eb', 'purple', 'gray', 'gold', '#ff6384'],
+            data: dataDash,
+            backgroundColor: colorDash,
           }
         ]
       },
@@ -82,14 +118,18 @@ export class DashboardComponent implements OnInit {
   }
   
   chartPieEstadoCivil(){
+    const labelDash = Object.keys(this.dash.results[0][2].user_civil_status)
+    const dataDash = Object.values(this.dash.results[0][2].user_civil_status)
+    const colorDash = Array.from({length: labelDash.length}, (v, k) => k+1).map( num => this.generateColor())
+
     const ctx = new Chart(this.myChartPieEstadoCivil.nativeElement, {
       type: 'pie',
       data: {
-        labels: ["Solteiro", "Casado", "Divorciado", "Noivo"],
+        labels: labelDash,
         datasets: [
           {
-            data: [12, 37, 6, 9],
-            backgroundColor: ['#36a2eb', '#CD104D', '#829460', '#F57328'],
+            data: dataDash,
+            backgroundColor: colorDash,
           }
         ]
       },
@@ -109,14 +149,18 @@ export class DashboardComponent implements OnInit {
   }
 
   chartPieGenero(){
+    const labelDash = Object.keys(this.dash.results[0][3].user_gender)
+    const dataDash = Object.values(this.dash.results[0][3].user_gender)
+    const colorDash = Array.from({length: labelDash.length}, (v, k) => k+1).map( num => this.generateColor())
+
     const ctx = new Chart(this.myChartPieGenero.nativeElement, {
       type: 'pie',
       data: {
-        labels: ["Masculino", "Feminino"],
+        labels: labelDash,
         datasets: [
           {
-            data: [63, 37],
-            backgroundColor: ['#36a2eb', '#ff6384'],
+            data: dataDash,
+            backgroundColor: colorDash,
           }
         ]
       },
